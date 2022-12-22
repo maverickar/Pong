2da02355b4fa6384af1fbdb8860aa9ad89f577c4
Maverick <83903931+maverickar@users.noreply.github.com>
2022-12-22 13:52:04 +0100

    Create PongGame.java

---
+++ PongGame.java
@@ -0,0 +1,14 @@
+package com.maverickar;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class PongGame {
+	
+	public static void main(String[] args) {
+		GameFrame frame = new GameFrame();
+	}
+		
+}
